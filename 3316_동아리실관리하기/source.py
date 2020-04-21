7aed6bd18dfcd21b043620a48837c219351ab68c
baeksangha <35217274+baeksangha@users.noreply.github.com>
2020-04-21 13:50:11 +0900

    3316 solved

--- 3316_동아리실관리하기/source.py
+++ 3316_동아리실관리하기/source.py
@@ -2,8 +2,24 @@ import sys
 
 
 def solution(manager):
-    print(manager)
-    return 0
+    bitmask = [[0 for _ in range(16)] for _ in range(len(manager))]
+    num = 1000000007
+    for i in range(16):
+        if i & 1 << 0 and i & (1 << (ord(manager[0]) - 65)):
+            bitmask[0][i] = 1
+
+    for i in range(1, len(manager)):
+        flag = 1 << (ord(manager[i]) - 65)
+        for j in range(16):
+            if flag & j:
+                for k in range(16):
+                    if j & k:
+                        bitmask[i][j] = (bitmask[i][j] + bitmask[i-1][k]) % num
+
+    answer = 0
+    for i in range(16):
+        answer = (answer + bitmask[-1][i]) % num
+    return answer
 
 
 if __name__ == "__main__":
