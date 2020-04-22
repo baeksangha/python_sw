2c19db59878bba92c6de2e20461741c17f79b10d
baeksangha <35217274+baeksangha@users.noreply.github.com>
2020-04-22 13:37:36 +0900

    1824 solved

--- 1824_혁진이의프로그램검증/source.py
+++ 1824_혁진이의프로그램검증/source.py
@@ -1,54 +1,46 @@
 import sys
 
 
-def solution(r, c, board, x, y, direction, route, value):
-    answer = "No"
-    compass = {'l': [0, -1], 'r': [0, 1], 'u': [-1, 0], 'd': [1, 0]}
-    dir = direction
-    while True:
-        token = board[x][y]
-        if '0' <= token <= '9':
-            value = int(token)
-        elif token in ['<', '>', '^', 'v']:
-            if token == '<':
-                dir = 'l'
-            elif token == '>':
-                dir = 'r'
-            elif token == '^':
-                dir = 'u'
-            else:
-                dir = 'd'
-        elif token == '_':
-            if value == 0:
-                dir = 'r'
-            else:
-                dir = 'l'
-        elif token == '|':
-            if value == 0:
-                dir = 'd'
-            else:
-                dir = 'u'
-        elif token == '+':
+def solution(r, c, dp, board):
+
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+    # x, y, direction, memory_value
+    queue = [(0, 0, 1, 0)]
+
+    while queue:
+        x, y, d, value = queue.pop()
+        dp[x][y][d][value] = 1
+        flag = board[x][y]
+        if "0" <= flag <= "9":
+            value = int(flag)
+        elif flag == '<' or (flag == '_' and value != 0):
+            d = 0
+        elif flag == '>' or (flag == '_' and value == 0):
+            d = 1
+        elif flag == '^' or (flag == '|' and value != 0):
+            d = 2
+        elif flag == 'v' or (flag == '|' and value == 0):
+            d = 3
+        elif flag == '+':
             value = (value + 1) % 16
-        elif token == '-':
+        elif flag == '-':
             value = (value - 1) % 16
-        elif token == '?':
-            for direction in ['u', 'r', 'd', 'l']:
-                new_x, new_y = ((x + compass[direction][0]) % r), ((y + compass[direction][1]) % c)
-                ret = solution(r, c, board, new_x, new_y, direction, route, value)
-                if ret == "Yes":
-                    return "Yes"
-            return "No"
-        elif token == '@':
-            answer = "Yes"
-            break
-        rec = (x, y, value, dir)
-        if rec in route:
-            break
+        elif flag == '@':
+            return "YES"
+
+        if flag == '?':
+            for i in range(4):
+                nx = (x + dx[i]) % r
+                ny = (y + dy[i]) % c
+                if not dp[nx][ny][i][value]:
+                    queue.append((nx, ny, i, value))
         else:
-            route.add((x, y, value, dir))
-        x, y = ((x + compass[dir][0]) % r), ((y + compass[dir][1]) % c)
-    return answer
+            nx = (x + dx[d]) % r
+            ny = (y + dy[d]) % c
+            if not dp[nx][ny][d][value]:
+                queue.append((nx, ny, d, value))
+    return "NO"
 
 
 if __name__ == "__main__":
@@ -56,5 +48,6 @@ if __name__ == "__main__":
     tests = int(input())
     for i in range(1, tests+1):
         r, c = map(int, input().split())
+        dp = [[[[0 for _ in range(16)] for _ in range(4)] for _ in range(c)] for _ in range(r)]
         board = [list(input()) for _ in range(r)]
-        print("#%d %s" % (i, solution(r, c, board, 0, 0, 'r', set(), 0)))
+        print("#%d %s" % (i, solution(r, c, dp, board)))
