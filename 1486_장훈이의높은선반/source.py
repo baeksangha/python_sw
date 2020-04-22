37eb5731774c81421d3e3381bc089252c8509ac9
baeksangha <35217274+baeksangha@users.noreply.github.com>
2020-04-22 17:23:16 +0900

    1486 solved

---
+++ 1486_장훈이의높은선반/source.py
@@ -0,0 +1,22 @@
+import sys
+from itertools import combinations
+
+
+def solution(n, b, talls):
+    answer = 200000
+    for i in range(1, n+1):
+        comb_list = list(map(list, (combinations(talls, i))))
+        for elem in comb_list:
+            p_sum = sum(elem)
+            if p_sum >= b:
+                answer = min(answer, p_sum)
+    return answer - b
+
+
+if __name__ == "__main__":
+    sys.stdin = open('input', 'r')
+    tests = int(input())
+    for i in range(1, tests+1):
+        n, b = map(int, input().split())
+        talls = list(map(int, input().split()))
+        print("#%d %d" % (i, solution(n, b, talls)))
