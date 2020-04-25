0006a86085ef8f9b89096a3e8821182db8872503
baeksangha <35217274+baeksangha@users.noreply.github.com>
2020-04-25 14:44:37 +0900

    1865 solved

---
+++ 1865_동철이의일분배/source.py
@@ -0,0 +1,39 @@
+import sys
+
+percents = []
+answer = 0
+
+
+def norm(a):
+    return int(a)/100
+
+
+def dfs(n, p, idx, visited):
+    global answer
+    if p <= answer:
+        return
+    if idx == n:
+        answer = max(answer, p)
+        return
+    for i in range(n):
+        if visited[i] == 0:
+            visited[i] = 1
+            dfs(n, p * percents[idx][i], idx+1, visited)
+            visited[i] = 0
+
+
+def solution(n):
+    global answer
+    answer = 0
+    visited = [0 for _ in range(n)]
+    dfs(n, 100, 0, visited)
+    return answer
+
+
+if __name__ == "__main__":
+    sys.stdin = open('input', 'r')
+    tests = int(input())
+    for i in range(1, tests+1):
+        n = int(input())
+        percents = [list(map(norm, input().split())) for _ in range(n)]
+        print("#%d %.6f" % (i, solution(n)))
