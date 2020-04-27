151b2de789503221191a960115e9ac3350eb5e9e
baeksangha <35217274+baeksangha@users.noreply.github.com>
2020-04-27 22:24:17 +0900

    3812 solved

--- 3812_호중이의큐브색칠/source.py
+++ 3812_호중이의큐브색칠/source.py
@@ -2,14 +2,31 @@ import sys
 
 
 def solution(x, y, z, a, b, c, n):
-    answer = 0
+    x_line = [0] * n
+    y_line = [0] * n
+    z_line = [0] * n
+    xy_line = [0] * n
+    xyz_line = [0] * n
 
-    return answer
+    for i in range(x):
+        x_line[abs(i-a) % n] += 1
+    for i in range(y):
+        y_line[abs(i-b) % n] += 1
+    for i in range(z):
+        z_line[abs(i-c) % n] += 1
+
+    for i in range(n):
+        for j in range(n):
+            xy_line[(i+j) % n] += x_line[i] * y_line[j]
+    for i in range(n):
+        for j in range(n):
+            xyz_line[(i+j) % n] += xy_line[i] * z_line[j]
+    return " ".join(list(map(str, xyz_line)))
 
 
 if __name__ == "__main__":
-    sys.stdin = open('input.txt', 'r')
+    sys.stdin = open('input', 'r')
     tests = int(input())
     for i in range(1, tests+1):
         x, y, z, a, b, c, n = map(int, input().split())
-        print("#%d %d" % (i, solution(x, y, z, a, b, c, n)))
+        print("#%d %s" % (i, solution(x, y, z, a, b, c, n)))
